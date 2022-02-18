3c522baf8d2aff4e1a7c53309ff7194178c2091a
Nirmal Naveen <53178630+NirmalNaveen20@users.noreply.github.com>
2022-02-18 17:35:29 +0530

    Update Demo.java

--- Lab02/src/Demo.java
+++ Lab02/src/Demo.java
@@ -5,15 +5,21 @@ public class Demo {
 		EvenOddNumber num = new EvenOddNumber();
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter number: ");
-		int var = sc.nextInt();
+		System.out.print("How many values do yo want to check: ");
+		int count = sc.nextInt();
 		
-		boolean result = num.findEvenOrOdd(var);
-		
-		if(result == true) {
-			System.out.println("Even Number");
-		} else {
-			System.out.println("Odd Number");
+		for(int i = 0; i < count; i++) {
+			System.out.print("Enter number: ");
+			int var = sc.nextInt();
+			
+			boolean result = num.findEvenOrOdd(var);
+			
+			if(result == true) {
+				System.out.println(var + " is a Even Number\n");
+			} else {
+				System.out.println(var + " is a Odd Number\n");
+			}
 		}
+		
 	}
 }
